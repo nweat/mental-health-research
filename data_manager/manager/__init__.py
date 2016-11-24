5961db046183671d3e3488e11250beb184f3eeea
nweat <nikkiweat@gmail.com>
2016-11-25 00:21:56 +0800

    Initial commit

---
+++ data_manager/manager/__init__.py
@@ -0,0 +1,1 @@
+from TweetManager import TweetManager
---
+++ data_manager/models/Keywords.py
@@ -0,0 +1,70 @@
+class Keywords:
+	
+	def __init__(self):
+
+		self.disease_list = [
+		'ptsd',
+		'anxiety',
+		'major depression',
+		'bipolar disorder',
+		'bpd',
+		'bipolar',
+		'seasonal affective disorder',
+		'psychotic depression',
+		'pmdd',
+		'situational depression',
+		'seasonal depression'
+		]
+
+		self.advocate_keywords = [
+		'mental health advocate',
+		'social worker',
+		'psychologist',
+		'doctor',
+		'clinical psychologist', 
+		'suicide prevention',
+		'endthestigma',
+		'end the stigma',
+		'researcher',
+		'health specialist',
+		'against depression',
+		'prevent depression',
+		'mental health supporter',
+		'mental health support',
+		'mental health professional',
+		'mental health activist',
+		'social activist'
+		]
+
+		self.depression_keywords = [
+		'depressed','depression','sad','lonely',
+		'tired','worthless','alone',
+		'pain', 'frustrated', 'depressed',
+		'anxiety','suicidal','broken','sadness',
+		'ugly','pain','death','fat','crying',
+		'cry','cutting','help','gay','severe',
+		'appetite','imissyou','severe','appetite',
+		'nausea','drowsiness','fatigue','addictive',
+		'attacks','episodes','anxious','frustrated',
+		'angry','attention seeking','isolated','fear',
+		'self absorbed','medication','meds','antidepressants',
+		'abuse','dissapointment','dissapointed','hiding',
+		'unfullfillment','self hate','panic attacks',
+		'trapped','repressed','miserable','rejected'
+		'abandoned','afraid','antisocial','crisis',
+		'crushed','despair','desperate','emotional',
+		'empty','hopeless','moody','overwhelmed',
+		'reckless','paranoid','self disgust','oppressed',
+		'needy','nervous','self-conscious','shattered',
+		'sobbing','sorrowful','suffering','tearful','touchy',
+		'uneasy','unhappy','unhinged','unpredictable','upset',
+		'vulnerable','wailing','weak','weepy','withdrawn','woeful',
+		'wounded','wretched','not understood','unwanted',
+		'ignored','not wanted','mentallillness','mental illness','mental health'
+		]
+
+		self.survivor_keywords = [
+		'survivor of mental illness','survivor of mentalillness','mentalillness survivor',
+		'survivor of depression'
+		]
+		#https://adarkershadeofblue.wordpress.com/2011/08/20/99-words-about-depression/
